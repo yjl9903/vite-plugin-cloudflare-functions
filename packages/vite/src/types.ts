d6273d1f82ad2ae3a60bd4bc350f851f790c527c
XLor <yjl9903@vip.qq.com>
2022-08-17 23:19:42 +0800

    docs: user config dir

--- packages/vite/src/types.ts
+++ packages/vite/src/types.ts
@@ -1,6 +1,10 @@
 export interface UserConfig {
   /**
    * Cloudflare Functions root directory
+   *
+   * This directory is relative to the Vite root
+   *
+   * @default './functions'
    */
   root?: string;
 
@@ -8,6 +12,10 @@ export interface UserConfig {
    * Copy the functions directory to outDir
    *
    * When it is undefined, plugin will not copy the directory
+   *
+   * This directory is relative to the Vite root
+   *
+   * @default false
    */
   outDir?: string | boolean;
 
