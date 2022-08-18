ca6d5baa7ec14fd69858235ed9cbaed5d03b8c2b
XLor <yjl9903@vip.qq.com>
2022-08-18 15:52:10 +0800

    chore!: rename server to worker

--- packages/vite-plugin-cloudflare-functions/build.config.ts
+++ packages/vite-plugin-cloudflare-functions/build.config.ts
@@ -1,7 +1,7 @@
 import { defineBuildConfig } from 'unbuild';
 
 export default defineBuildConfig({
-  entries: ['src/index', 'src/server', 'src/client'],
+  entries: ['src/index', 'src/worker', 'src/client'],
   declaration: true,
   clean: true,
   rollup: {
--- playground/functions/api/[msg].ts
+++ playground/functions/api/[msg].ts
@@ -1,3 +1,3 @@
-import { makeResponse } from 'vite-plugin-cloudflare-functions/server';
+import { makeResponse } from 'vite-plugin-cloudflare-functions/worker';
 
 export const onRequestGet = () => makeResponse({ status: 'OK', data: 'Hello' });
