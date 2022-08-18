d4e78b76534f25ad02c3e6a25f049b5eb41785f3
XLor <yjl9903@vip.qq.com>
2022-08-18 14:32:12 +0800

    chore: display info

--- playground/app/main.ts
+++ playground/app/main.ts
@@ -1,7 +1,10 @@
+import now from '~build/time';
+
+document.querySelector('#info')!.innerHTML = 'Build at: ' + now.toLocaleString();
+
 const endpoint = '/api/hello';
 
 fetch(endpoint).then(async (data) => {
-  document.write(`Receive from Pages Functions (${endpoint}): ` + (await data.text()));
+  document.querySelector('#app')!.innerHTML =
+    `Receive from Pages Functions (${endpoint}): ` + (await data.text());
 });
-
-export {};
--- playground/app/vite.config.ts
+++ playground/app/vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite';
 
+import Info from 'vite-plugin-info';
 import CloudflarePagesFunctions from 'vite-plugin-cloudflare-functions';
 
 export default defineConfig({
   plugins: [
+    Info(),
     CloudflarePagesFunctions({ root: '../functions', outDir: '../../', wrangler: { log: true } })
   ]
 });
